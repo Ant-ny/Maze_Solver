ee00a0f259e7d5a7e2827d8381a897893498771a
ant-ny <popadopalus83@gmail.com>
2024-02-01 05:52:52 -0500

    added draw_move method

--- cells.py
+++ cells.py
@@ -31,3 +31,19 @@ class Cell:
         if self.has_top_wall:
             bottom_l = Line(Point(self._x2, self._y1), Point(self._x1, self._y1))
             self._win.draw_line(bottom_l)
+
+    def draw_move(self, to_cell, undo=False):
+        center_x1 = (self._x1 + self._x2) / 2
+        center_y1 = (self._y1 + self._y2) / 2
+        center_x2 = (to_cell._x1 + to_cell._x2) / 2
+        center_y2 = (to_cell._y1 + to_cell._y2) / 2
+        if undo is False:
+            point1 = Point(center_x1, center_y1)
+            point2 = Point(center_x2, center_y2)
+            line = Line(point1, point2)
+            self._win.draw_line(line, "red")
+        elif undo:
+            point1 = Point(center_x1, center_y1)
+            point2 = Point(center_x2, center_y2)
+            line = Line(point1, point2)
+            self._win.draw_line(line, "gray")
