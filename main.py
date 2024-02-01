83c46dc5834cc3e995233bec9796c6f64f50ed17
ant-ny <popadopalus83@gmail.com>
2024-02-01 03:57:21 -0500

    added ability to draw lines

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(100, 100), Point(700, 500))
+    win.draw_line(line, "black")
     win.wait_for_close()
 
 main()
